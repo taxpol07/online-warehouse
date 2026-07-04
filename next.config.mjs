8a57f29b236ee241af648a934b2efc6fc86223bf
taxpol07 <polatcan.keser@icloud.com>
2026-07-04 17:41:56 +0300

    hhh

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,8 @@
-import withPWAInit from "next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development", // Geliştirme ortamında (localhost) kapalı tutuyoruz
-  register: true,
-  skipWaiting: true,
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Vercel build aşamasında TS hatalarını yoksayar
     ignoreBuildErrors: true,
   },
-  // YENİ: Turbopack hatasını susturur ve sitenin çok hızlı açılmasını sağlar
-  turbopack: {}, 
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
